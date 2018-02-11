8dffd8938b3ec2581ebe9db3b81d63ca71cd4001
Sevren <kgoguev@gmail.com>
2018-02-12 00:36:15 +0100

    Added summary on dstservermanager , added todos Moved some constant strings to header file

--- dstservermanager.h
+++ dstservermanager.h
@@ -47,6 +47,23 @@ private:
 	void setupConnections();
 	void dstServerManager::setupMainLayout();
 	std::vector<std::tuple<int,string,string,string>> dstServerManager::fillGameOptionsArray(const char *);
+
+	// constants for resources and xpath queries
+	const char* imgPath = ".//Resources//imgs//worldsettings.png";
+	const char* worldSettingsXmlPath = ".//Resources//worldSettings.xml";
+	const char* forestWorldCountsXpath = "count(//Presets/Forest/World/*)";
+	const char* forestResourcesCountsXpath = "count(//Presets/Forest/Resources/*)";
+	const char* forestFoodCountsXpath = "count(//Presets/Forest/Food/*)";
+	const char* forestAnimalsCountsXpath = "count(//Presets/Forest/Animals/*)";
+	const char* forestMonstersCountsXpath = "count(//Presets/Forest/Monsters/*)";
+	//
+	const char* cavesWorldCountsXpath = "count(//Presets/Cave/World/*)";
+	const char* cavesResourcesCountsXpath = "count(//Presets/Cave/Resources/*)";
+	const char* cavesFoodCountsXpath = "count(//Presets/Cave/Food/*)";
+	const char* cavesAnimalsCountsXpath = "count(//Presets/Cave/Animals/*)";
+	const char* cavesMonstersXpath = "count(//Presets/Cave/Monsters/*)";
+
+
 	
 	
 };
