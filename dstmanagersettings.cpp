09c0f8dea72d9b7c6678f5d1e3fb4956671d73b4
Sevren <kgoguev@gmail.com>
2018-02-12 00:25:09 +0100

    Added summary and todos for dstmanagersettings

--- dstmanagersettings.cpp
+++ dstmanagersettings.cpp
@@ -4,6 +4,18 @@
 #include "dstservermanager.h"
 #include <qfiledialog.h>
 
+/*
+* DstManagerSettings - This form holds the overall configuration for DSTServerManager
+* Responsible for:
+*  1. settings the default path to where the server configuration files live. 
+*  2. providing the path to SteamCMD 
+*  3. providing the ServerToken
+*  !! Must be set up at least once! reads the configuration values from a dstmanagersettings ini file.!!
+*/
+
+//TODO: Read settings from dstmanagersettings ini file
+//TODO: Provide functions to retrieve default server file path, default path to steamCMD and serverToken
+//TODO: Setup fileChooser to select the path for configuration files and the steamCMD.
 
 dstmanagersettings::dstmanagersettings(dstServerManager *dstWindow, QWidget *parent)
 	: QWidget(parent)
