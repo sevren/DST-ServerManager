6fe14157398e9858e043acc0e9e0e0375d2e79a9
Sevren <kgoguev@gmail.com>
2018-03-11 17:37:02 +0100

    added custom datatype header

---
+++ dstman_customdatatypes.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+
+/*
+* Intended to hold all the custom data types used in the dst manager.
+*/
+
+struct GlobalDSTManSettings
+{
+	std::string defaultServerFilePath;
+	std::string steamCMDExecutablePath;
+	std::string clusterTokenValue;
+
+};
